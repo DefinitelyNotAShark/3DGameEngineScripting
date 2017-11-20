353c22d450ddf8cbcdf521c4621ed01531747703
DefinitelyNotAShark <32074072+DefinitelyNotAShark@users.noreply.github.com>
2017-11-20 15:18:20 -0600

    Did project imports

---
+++ Assets/Scripts/IActivatable.cs
@@ -0,0 +1,6 @@
+﻿//monobehavior allows it to be dragged onto gameobjects
+public interface IActivatable {
+    //you can call this function on any activatable object. 
+    //The switch has an activate function, and so does the door and the collectable
+    void DoActivate();
+}
